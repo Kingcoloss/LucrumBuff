eda6fcbe88c2f4acc49dec22299e48c2d242a60d
kingcoloss <sridung1111@gmail.com>
2022-04-16 23:57:12 +0700

    rebaseProject

---
+++ layouts/NavTab.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    <nav class="navbar sticky-top">
+      <div class="container-fluid">
+        <a class="navbar-brand" href="#">NavbarTitle</a>
+      </div>
+    </nav>
+    <div class="container">
+      <Nuxt />
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+}
+</script>
+
+<style lang="scss" scoped>
+</style>
--- pages/index.vue
+++ pages/index.vue
@@ -5,5 +5,6 @@
 <script>
 export default {
   name: 'IndexPage',
+  layout: 'NavTab'
 }
 </script>
